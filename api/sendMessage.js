46c2380c98467a807941bfaec3862b9032b6797c
infocssmdana <info.cs.mdana@gmail.com>
2025-11-04 10:56:25 +0700

    Create sendMessage.js

---
+++ api/sendMessage.js
@@ -0,0 +1,25 @@
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const token = process.env.TELEGRAMBOT;
+    const chatId = process.env.TELEGRAM_CHAT;
+    const { text } = req.body;
+
+    const response = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text,
+      }),
+    });
+
+    if (!response.ok) throw new Error("Gagal kirim ke Telegram");
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
